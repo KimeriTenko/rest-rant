8035260c3d3197a1d824d815fe879ea535530051
Mary Frye <maryfrye@Marys-MacBook-Pro.local>
2022-07-19 12:48:50 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(3000)
+
+
+/*GET   /Homepage*/
+
+/*GET   /places   Index page listing all places/*
+
+/*GET   /places/new   Form to create a new place*/
+
+/*POST   /places   Create a new place*/
+
+/*GET   /places/:id   Show page containing details for a specific place (including a comment form to post rants)*/
+
+/*GET   /places/:id/edit   Edit form for a place*/
+
+/*PUT   /places/:id   Make changes to an existing place*/
+
+/*DELETE   /places/:id   Delete an existing place*/
+
+/*POST   /places/:id/rant   Post a rant (comment) about a specific place*/
+
+/*DELETE    /places/:id/rant/:id    Delete an existing rant (comment)*/ 
