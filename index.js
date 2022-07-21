34836b183c8ee7830d1d5b9b7248ed81c7120bd8
Mary Frye <maryfrye@Marys-MacBook-Pro.local>
2022-07-20 20:00:10 -0400

    edit to index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 /*GET   /Homepage*/
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.sender('home')
 })
 
 /*GET 404 Page*/
