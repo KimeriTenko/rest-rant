0c42a86514e05d45596b063dddfca5a08a94d28c
Mary Frye <maryfrye@Marys-MacBook-Pro.local>
2022-07-26 11:46:23 -0400

    EDIT index.js and show.jsx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*Globals and Modules*/
 require('dotenv').config()
 const express = require('express')
-// const methodOverride = require('method-override')
+const methodOverride = require('method-override')
 const app = express()
 
 /*Express Settings*/
@@ -10,11 +10,10 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 
 /*Controllers and Routes*/
 app.use('/places', require('./controllers/places'))
-
 
 /*GET places*/
 app.use('/places', require('./controllers/places'))
