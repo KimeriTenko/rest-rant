74d31c7fadbcf0e07f8de083ab2a757f91bb30a7
Mary Frye <deimospho@gmail.com>
2022-08-04 15:59:50 -0400

    NEW seed-commments.js, seeed-places.js, EDITsdefault.jsx, show.jsx, new.jsx, error404.jsx

--- views/default.jsx
+++ views/default.jsx
@@ -1,14 +1,18 @@
 const React = require('react')
 
 function Def (html) {
-        return (
-            <html>
-                <head>
-                    <title>Title</title>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
-                    <link rel="stylesheet" href="/css/style.css"/>
-                </head>
-                <body>
+    return (
+        <html>
+            <head>
+                <title>Rest-RANT</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+                <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest"></link>
+            </head>
+            <body>
                 <nav>
                     <ul>
                         <li>
@@ -22,10 +26,10 @@ function Def (html) {
                         </li>
                     </ul>
                 </nav>
-                    {html.children}
-                </body>
-            </html>
-        )
-      }      
+                {html.children}
+            </body>
+        </html>
+    )
+}      
 
 module.exports = Def
--- views/error404.jsx
+++ views/error404.jsx
@@ -8,10 +8,9 @@ function error404 () {
       <main>
         <h1>404: PAGE NOT FOUND</h1>
         <p>Oops, sorry, we can't find this page!</p>
-        <img src="/images/oops-girl.jpg" alt="Cat that is very sorry he can't find your page" />
-        {/* <img src="/images/oops-girl.jpg" alt="Girl that is very sorry she can't find your page" />
+        <img src="/images/oops-girl.jpg" alt="Girl that is very sorry she can't find your page" />
         <div>Photo by <a href="https://unsplash.com/@ilumire?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jelleke Vanooteghem</a> on <a href="https://unsplash.com/s/photos/oops?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        </div> */}
+        </div>
       </main>
     </Def>
   )
